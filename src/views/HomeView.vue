f74bb3bfd9dcd16927341457346af3629de404ab
JasonMH <jasonmonzon03@gmail.com>
2024-10-26 10:21:18 -0400

    updating

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { reactive } from 'vue'
 
+const cards = reactive([
+  { show: false },
+  { show: false },
+  { show: false },
+  { show: false },
+  { show: false },
+  { show: false }
+])
+
+function toggle(index) {
+  cards[index].show = !cards[index].show
+}
 </script>
 
 <template>
@@ -33,6 +46,7 @@
     <v-row align="center" justify="center">
       <v-col class="text-center">
         <h1>Bienvenido a JaMonzon®</h1>
+        <v-divider></v-divider>
         <h3>Tienda especializada en la venta de productos alimenticios</h3>
       </v-col>
     </v-row>
@@ -42,23 +56,71 @@
 
   <v-carousel show-arrows="hover">
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    src="../img/bacon.jpeg"
     cover
   ></v-carousel-item>
 
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+    src="../img/jamon.jpg"
     cover
   ></v-carousel-item>
 
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    src="../img/queso.webp"
     cover
   ></v-carousel-item>
   
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    src="../img/salsas.jpg"
     cover
   ></v-carousel-item>
 </v-carousel>
+
+<v-divider></v-divider>
+<v-divider></v-divider>
+<v-divider></v-divider>
+<br>
+
+
+<v-container>
+    <v-row>
+      <v-col
+        v-for="(card, index) in cards"
+        :key="index"
+        cols="12"
+        md="4"
+      >
+        <v-card class="mx-auto" max-width="344">
+          <v-img
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            cover
+          ></v-img>
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+          <v-card-subtitle>
+            1,000 miles of wonder
+          </v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange-lighten-2" text="Explore"></v-btn>
+            <v-spacer></v-spacer>
+            <v-btn :icon="card.show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="toggle(index)"></v-btn>
+          </v-card-actions>
+          <v-expand-transition>
+            <div v-show="card.show">
+              <v-divider></v-divider>
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <br>
+  <br>
+
 </template>
