590d8cdb2d74efd3639c0d6ec63b1f42aa9e26cb
JasonMH <jasonmonzon03@gmail.com>
2024-10-27 18:19:37 -0400

    buying

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -20,7 +20,7 @@
     </v-list>
     <div class="d-flex justify-end mt-4">
       <v-btn rounded="lg" color="grey" @click="goBack" class="back">Volver</v-btn>
-      <v-btn rounded="lg" color="green"   class="back" >Comprar</v-btn>
+      <v-btn rounded="lg" color="green" @click="sendMessage"  class="back" >Comprar</v-btn>
     </div>
   </v-container>
 </template>
@@ -33,6 +33,21 @@ import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const cart = ref(JSON.parse(route.query.cart || '[]'));
+
+const sendMessage = () => {
+  const phoneNumber = '+5356098551'; // Reemplaza con el número de teléfono deseado
+  const message = generateMessage(cart.value);
+  
+  const encodedMessage = encodeURIComponent(message);
+  const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+  // Abre el enlace en una nueva ventana
+  window.open(url, '_blank');
+};
+
+const generateMessage = (data) => {
+  return data.map(item => `${item.title} - Precio: ${item.subtitle}`).join('\n');
+};
 
 const props = defineProps(['cart']);
 const router = useRouter();
