af741f5ebefbb72ecb4d3a3167dd3eb1d9a1e832
Tay <taimarytrimino@gmail.com>
2024-10-27 17:50:28 -0400

    cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,20 +1,26 @@
 <template>
   <v-container>
-    <h1 class="compras">Carrito de Compras</h1>
+    <h1 class="compras text-center">Carrito de Compras</h1>
     <v-list>
       <v-list-item-group>
         <v-list-item
           v-for="(product, index) in cart"
           :key="index"
+          class="d-flex justify-space-between align-center"
         >
           <v-list-item-content>
             <v-list-item-title>{{ product.title }}</v-list-item-title>
             <v-list-item-subtitle>{{ product.subtitle }}</v-list-item-subtitle>
           </v-list-item-content>
+          <v-btn rounded="xl" size="x-small" color="red darken-3" elevation="0">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
         </v-list-item>
       </v-list-item-group>
     </v-list>
-    <v-btn @click="goBack" class="back">Volver</v-btn>
+    <div class="d-flex justify-end mt-4">
+      <v-btn @click="goBack" class="back">Volver</v-btn>
+    </div>
   </v-container>
 </template>
 
